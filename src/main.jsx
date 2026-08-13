2c9590e02635ffd5e5f51d002ab3903ba4fe422f
Muntajir Alam <muntajiralam20074@gmail.com>
2026-08-14 00:15:08 +0530

    feat: add Lenis smooth scrolling functionality and integrate it into the main application

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,21 @@ import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import store from './store';
+import useLenis from './shared/hooks/useLenis';
 import './index.css';
+
+function LenisProvider({ children }) {
+  useLenis();
+  return children;
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        <App />
+        <LenisProvider>
+          <App />
+        </LenisProvider>
       </BrowserRouter>
     </Provider>
   </React.StrictMode>
